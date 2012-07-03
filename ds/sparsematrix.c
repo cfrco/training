027853eaa6a4ecf27e37af2668ea0d8e6e85976c
cat <z82206.cat@gmail.com>
2012-07-03 19:42:58 +0800

    sparsematrix add

--- ds/sparsematrix.c
+++ ds/sparsematrix.c
@@ -2,6 +2,11 @@
 #include<stdlib.h>
 
 #define MAX_LENGTH 100
+
+#define GET_ROW(_sm,_i) ((_sm)->entries[(_i)].row)
+#define GET_COL(_sm,_i) ((_sm)->entries[(_i)].col)
+#define GET_VALUE(_sm,_i) ((_sm)->entries[(_i)].value)
+
 typedef struct entry
 {
     int row,col;
@@ -51,7 +56,79 @@ void transpose(spmatrix *s,spmatrix *d)
 
 void add(spmatrix *a,spmatrix *b,spmatrix *c)
 {
+    int la = a->length;
+    int lb = b->length;
+    int lc = 0;
 
+    int i,j;
+
+    for(i=0,j=0;i<la&&j<lb;)
+    {
+        if(GET_ROW(a,i) == GET_ROW(b,j))
+        {
+            if(GET_COL(a,i) == GET_COL(b,j))
+            {
+                GET_ROW(c,lc) = GET_ROW(a,i);
+                GET_COL(c,lc) = GET_COL(a,i);
+                GET_VALUE(c,lc) = GET_VALUE(a,i)+GET_VALUE(b,j);
+                ++lc;
+                ++i;
+                ++j;
+            }
+            else if(GET_COL(a,i) > GET_COL(b,j))
+            {
+                GET_ROW(c,lc) = GET_ROW(b,j);
+                GET_COL(c,lc) = GET_COL(b,j);
+                GET_VALUE(c,lc) = GET_VALUE(b,j);
+                ++lc;
+                ++j;
+            }
+            else if(GET_COL(a,i) < GET_COL(b,j))
+            {
+                GET_ROW(c,lc) = GET_ROW(a,i);
+                GET_COL(c,lc) = GET_COL(a,i);
+                GET_VALUE(c,lc) = GET_VALUE(a,i);
+                ++lc;
+                ++i;
+            }
+        }
+        else if(GET_ROW(a,i) > GET_ROW(b,j))
+        {
+            GET_ROW(c,lc) = GET_ROW(b,j);
+            GET_COL(c,lc) = GET_COL(b,j);
+            GET_VALUE(c,lc) = GET_VALUE(b,j);
+            ++lc;
+            ++j;
+        }
+        else
+        {
+            GET_ROW(c,lc) = GET_ROW(b,j);
+            GET_COL(c,lc) = GET_COL(b,j);
+            GET_VALUE(c,lc) = GET_VALUE(b,j);
+            ++lc;
+            ++j;
+        }
+    }
+
+    for(;i<la;++i)
+    {
+        GET_ROW(c,lc) = GET_ROW(a,i);
+        GET_COL(c,lc) = GET_COL(a,i);
+        GET_VALUE(c,lc) = GET_VALUE(a,i);
+        ++lc;
+    }
+
+    for(;j<lb;++j)
+    {
+        GET_ROW(c,lc) = GET_ROW(b,j);
+        GET_COL(c,lc) = GET_COL(b,j);
+        GET_VALUE(c,lc) = GET_VALUE(b,j);
+        ++lc;
+    }
+
+    c->length = lc;
+    c->row = a->row;
+    c->col = a->col;
 }
 
 void spmatrix2matrix(spmatrix *s,int *m,int *r,int *c)
@@ -132,5 +209,25 @@ int main()
     spmatrix2matrix(&sm2,m,&r,&c);
     print_matrix(m,r,c);
 
+    scanf("%d %d",&r,&c);
+    for(i=0;i<r;++i)
+        for(j=0;j<c;++j)
+            scanf("%d",m+i*c+j);
+
+    matrix2spmatrix(m,r,c,&sm);
+    print_spmatrix(&sm);
+    puts("=============");
+    scanf("%d %d",&r,&c);
+    for(i=0;i<r;++i)
+        for(j=0;j<c;++j)
+            scanf("%d",m+i*c+j);
+
+    matrix2spmatrix(m,r,c,&sm2);
+    print_spmatrix(&sm2);
+    puts("=============");
+
+    add(&sm,&sm2,&sm3);    
+    spmatrix2matrix(&sm3,m,&r,&c);
+    print_matrix(m,r,c);
     return 0;
 }
